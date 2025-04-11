fb296bfe6ecc04268766b60a432a88d780fdddb3
Sajan Sthapit <sajan.sthapit@bitskraft.com>
2025-04-11 20:33:15 +0545

    fix: vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -9,5 +9,6 @@ const prisma =
 	globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+else  globalForPrisma.prisma = new PrismaClient();
 
 export default prisma;
