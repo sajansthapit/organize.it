b9ba7acc3aad6a8fc9d4c1c3a85c48b2b7224f69
Sajan Sthapit <sajan.sthapit@bitskraft.com>
2025-04-11 19:06:37 +0545

    fix: vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
-import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	transpilePackages: ["@workspace/ui"],
 	webpack: (config, { isServer }) => {
-		// if (isServer) {
-		// 	config.externals.push({ encoding: "commonjs encoding" });
-		// }
 		if (isServer) {
-			config.plugins = [...config.plugins, new PrismaPlugin()];
+			config.externals.push({ encoding: "commonjs encoding" });
 		}
 		return config;
 	},
